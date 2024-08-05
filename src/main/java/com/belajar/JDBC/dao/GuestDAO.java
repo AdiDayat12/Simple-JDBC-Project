98ad41bdfaab1f87d3caa2d63a0e034453a63d81
LinAn <adilinan72@gmail.com>
2024-08-05 23:58:03 +0300

    Added search, update, and delete methods and fixed add method

--- src/main/java/com/belajar/JDBC/dao/GuestDAO.java
+++ src/main/java/com/belajar/JDBC/dao/GuestDAO.java
@@ -2,29 +2,158 @@ package com.belajar.JDBC.dao;
 
 import com.belajar.JDBC.model.Guest;
 import com.belajar.JDBC.util.ConnectionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class GuestDAO {
+    Logger log = LoggerFactory.getLogger(GuestDAO.class);
+
     public void addGuest (Guest guest){
         String sql = """
-                INSERT INTO guest (first_name, last_name, date_of_birth, email, tel_no) VALUES
-                (?, ?, ?, ?, ?)
+                INSERT INTO guest (first_name, last_name, date_of_birth, email, tel_no) VALUES (?, ?, ?, ?, ?);
                 """;
 
         try (Connection conn = ConnectionUtil.dataSource.getConnection();
-             PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-            preparedStatement.setString(1, guest.getFirst_name());
-            preparedStatement.setString(2, guest.getLast_name());
-            preparedStatement.setDate(3, Date.valueOf(guest.getDateOfBirth()));
-            preparedStatement.setString(4, guest.getEmail());
-            preparedStatement.setString(5, guest.getTel_no());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, guest.getFirst_name());
+            ps.setString(2, guest.getLast_name());
+            ps.setDate(3, Date.valueOf(guest.getDateOfBirth()));
+            ps.setString(4, guest.getEmail());
+            ps.setString(5, guest.getTel_no());
+
+            ps.executeUpdate();
+        }catch (SQLException e){
+            log.error("Error executing addGuest", e);
             throw new RuntimeException(e);
         }
+    }
+    public List<Guest> searchGuest (String keyword){
+        List<Guest> guestList = new ArrayList<>();
+        String sql = """
+                SELECT * FROM guest WHERE first_name ILIKE ? OR last_name ILIKE ? OR email ILIKE ?\s
+                \s""";
+
+        try (Connection conn = ConnectionUtil.dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)){
+            keyword = "%" + keyword + "%";
+
+            ps.setString(1, keyword);
+            ps.setString(2, keyword);
+            ps.setString(3, keyword);
+
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()){
+                Guest guest = new Guest();
+                guest.setFirst_name(resultSet.getString("first_name"));
+                guest.setLast_name(resultSet.getString("last_name"));
+                guest.setDateOfBirth(resultSet.getDate("date_of_birth").toLocalDate());
+                guest.setEmail(resultSet.getString("email"));
+                guest.setTel_no(resultSet.getString("tel_no"));
+
+                guestList.add(guest);
+            }
+
+        } catch (SQLException e){
+            log.error("Error executing searchGuest", e);
+            throw new RuntimeException(e);
+        }
+        return guestList;
+    }
+
+    public List<Guest> getAllGuests (){
+        List<Guest> guestList = new ArrayList<>();
+        String sql = """
+                SELECT * FROM guest""";
+
+
+        try (Connection conn = ConnectionUtil.dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)){
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()){
+                Guest guest = new Guest();
+                guest.setFirst_name(resultSet.getString("first_name"));
+                guest.setLast_name(resultSet.getString("last_name"));
+                guest.setDateOfBirth(resultSet.getDate("date_of_birth").toLocalDate());
+                guest.setEmail(resultSet.getString("email"));
+                guest.setTel_no(resultSet.getString("tel_no"));
+
+                guestList.add(guest);
+            }
+        } catch (SQLException e){
+            log.error("Error ", e);
+            throw new RuntimeException(e);
+        }
+        return guestList;
+    }
+
+    public int updateGuest(String column, String prevData, String newData){
+        List<String> validColumns = Arrays.asList("first_name", "date_of_birth", "last_name", "email", "tel_no");
+        if (!validColumns.contains(column)){
+            log.error("Error ", new Throwable());
+        }
+        if (column.equals("date_of_birth")){
+            return updateDate(prevData, newData);
+        }
+        String sql = "UPDATE guest SET " + column + " = ? WHERE " + column + " = ?";
+        int result = 0;
+        try (Connection conn = ConnectionUtil.dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, newData);
+            ps.setString(2, prevData);
+            
+            result = ps.executeUpdate();
+        } catch (SQLException e){
+            log.error("Error", new Throwable());
+        }
+        return result;
+    }
+
+    private int updateDate(String prevDate, String newDate) {
+        Date date1 = parseToDate(prevDate);
+        Date date2 = parseToDate(newDate);
+
+        String sql = "UPDATE guest SET date_of_birth = ? WHERE date_of_birth = ?";
+        int result = 0;
+        try (Connection conn = ConnectionUtil.dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setDate(1, date2);
+            ps.setDate(2, date1);
+            result = ps.executeUpdate();
+        } catch (SQLException e) {
+            log.error("Error updating date", e);
+        }
+        return result;
+    }
+
+    private Date parseToDate(String date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return new java.sql.Date(sdf.parse(date).getTime());
+        } catch (ParseException e) {
+            throw new RuntimeException("Invalid date format", e);
+        }
+    }
+
+    public int deleteGuest (String keyword){
+        String sql = """
+                DELETE FROM guest WHERE first_name = ? OR last_name = ?""";
+        int result = 0;
+        try (Connection conn = ConnectionUtil.dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, keyword);
+            ps.setString(2, keyword);
+            result = ps.executeUpdate();
+        }catch (SQLException e){
+            log.error("Error ", new Throwable());
+        }
+        return result;
     }
 }
--- src/test/java/com/belajar/JDBC/GuestDAOTest.java
+++ src/test/java/com/belajar/JDBC/GuestDAOTest.java
@@ -4,44 +4,64 @@ import com.belajar.JDBC.dao.GuestDAO;
 import com.belajar.JDBC.model.Guest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuestDAOTest {
-    private GuestDAO newGuest;
+
+    Logger log  = LoggerFactory.getLogger(GuestDAOTest.class);
     private Guest guest;
+    private GuestDAO guestDAO;
 
     @BeforeEach
     void setUp (){
-        guest = new Guest();
-        guest.setFirst_name("Adi");
-        guest.setLast_name("linan");
-        guest.setDateOfBirth(LocalDate.of(1990, 5, 23));
-        guest.setEmail("adi@gmail.com");
-        guest.setTel_no("1234567890123");
+        guestDAO = new GuestDAO();
     }
-
-    @Test
-    void testGuest (){
-        assertEquals("Adi", guest.getFirst_name());
-        assertEquals("linan", guest.getLast_name());
-        assertEquals(LocalDate.of(1990, 5, 23), guest.getDateOfBirth());
-        assertEquals("adi@gmail.com", guest.getEmail());
-        assertEquals("1234567890123", guest.getTel_no());
-    }
-
-    @Test
-    void testObj (){
-        assertNotNull(guest);
-    }
-
     @Test
     void insertData (){
-        newGuest = new GuestDAO();
-        newGuest.addGuest(guest);
+        assertNotNull(guest);
+        guestDAO.addGuest(guest);
     }
 
+    @Test
+    void searchData (){
+        List<Guest> result = guestDAO.searchGuest("lin");
+        printResults(result);
+    }
+
+    @Test
+    void getAllData (){
+        List<Guest> result = guestDAO.getAllGuests();
+        printResults(result);
+    }
+
+    @Test
+    void updateFirstName (){
+        int hasil = guestDAO.updateGuest("first_name",  "Muhtar", "ADI");
+        assertEquals(2, hasil);
+    }
+
+    private void printResults(List<Guest> guests) {
+        System.out.println("====== Result ======");
+        guests.forEach(v -> System.out.println(v.getFirst_name() + " " + v.getLast_name() + " " + v.getDateOfBirth() + " - "  +  v.getEmail() + " - " + v.getTel_no()));
+    }
+
+    @Test
+    void updateDateTest (){
+        int hasil = guestDAO.updateGuest("date_of_birth", "1990-05-23", "2000-12-12");
+        assertEquals(2, hasil);
+    }
+
+    @Test
+    void deleteTest (){
+        int hasil = guestDAO.deleteGuest("ADI");
+        assertEquals(2, hasil);
+    }
 }
